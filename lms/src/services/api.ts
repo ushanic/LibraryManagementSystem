003bf074931069d3a67e7d408dcb6ad6c48adfd3
H D U Chathushka <hdchathushka@gmail.com>
2025-12-18 18:11:32 +0530

    change the port

--- lms/src/services/api.ts
+++ lms/src/services/api.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
 import type { Book } from "../types/Book";
 
+const API_URL = "http://localhost:5243/api/books";
 
-const API_URL = "https://localhost:5001/api/books";
+
 
 export const getBooks = () => axios.get<Book[]>(API_URL);
 export const addBook = (book: Book) => axios.post(API_URL, book);
-export const updateBook = (id: number, book: Book) => axios.put(`${API_URL}/${id}`, book);
-export const deleteBook = (id: number) => axios.delete(`${API_URL}/${id}`);
+export const updateBook = (id: number, book: Book) =>
+
+  axios.put(`${API_URL}/${id}`, book);
+export const deleteBook = (id: number) =>
+  axios.delete(`${API_URL}/${id}`);
