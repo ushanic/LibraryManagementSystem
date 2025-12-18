672d9fab87acc673549df6efeb210a50054f2a42
H D U Chathushka <hdchathushka@gmail.com>
2025-12-18 19:02:26 +0530

    did some changes

--- lms/src/components/BookTable.tsx
+++ lms/src/components/BookTable.tsx
@@ -15,7 +15,7 @@ const BookTable: React.FC<Props> = ({ books, onEdit, onDelete }) => {
           <th className="border p-2">Image</th>
           <th className="border p-2">Title</th>
           <th className="border p-2">Author</th>
-          <th className="border p-2 w-70">Description</th>
+          <th className="border p-2 w-72">Description</th>
           <th className="border p-2">Rack</th>
           <th className="border p-2">Status</th>
           <th className="border p-2">Actions</th>
@@ -33,22 +33,25 @@ const BookTable: React.FC<Props> = ({ books, onEdit, onDelete }) => {
             </td>
             <td className="border p-2">{book.title}</td>
             <td className="border p-2">{book.author}</td>
-            <td className="border p-2 w-70 break-words">{book.description}</td>
+            <td className="border p-2 w-72 break-words">{book.description}</td>
             <td className="border p-2">{book.rackNumber}</td>
             <td className="border p-2">{book.status}</td>
-            <td className="border p-2 flex flex-col items-center gap-2">
-              <button
-                className="bg-black text-white p-1 rounded w-20"
-                onClick={() => onEdit(book)}
-              >
-                Edit
-              </button>
-              <button
-                className="bg-orange-700 text-white p-1 rounded w-20"
-                onClick={() => onDelete(book.id)}
-              >
-                Delete
-              </button>
+            <td className="border p-2">
+              {/* Use flex with gap horizontally to match row height */}
+              <div className="flex justify-center items-center gap-2 h-full">
+                <button
+                  className="bg-black text-white px-2 py-1 rounded"
+                  onClick={() => onEdit(book)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="bg-orange-700 text-white px-2 py-1 rounded"
+                  onClick={() => onDelete(book.id)}
+                >
+                  Delete
+                </button>
+              </div>
             </td>
           </tr>
         ))}
